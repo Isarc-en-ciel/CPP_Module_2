80ac8778e621186d4359d0fa7de7850d62d9c9ec
iwaslet <iwaslet@student.42.fr>
2025-12-11 20:43:06 +0100

    Cpp

--- Cpp06/ex01/main.cpp
+++ Cpp06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iwaslet <iwaslet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 17:20:41 by iwaslet           #+#    #+#             */
-/*   Updated: 2025/11/28 16:13:05 by iwaslet          ###   ########.fr       */
+/*   Updated: 2025/12/11 20:42:27 by iwaslet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int	main()
 	int a = 25;
 	Data d= {&a};
 	uintptr_t raw = Serializer::serialize(&d);
-	std::cout << d.ptr << " : " <<*(int*)(d.ptr) << std::endl;
+	std::cout << &d << " : " <<*(int*)(d.ptr) << std::endl;
 	std::cout <<"raw: " << raw << std::endl;
 	std::cout << Serializer::deserialize(raw) << std::endl;
 	Data *D =	Serializer::deserialize(raw);
-	std::cout << D->ptr << " : " <<*(int*)(D->ptr) << std::endl;
+	std::cout << D << " : " <<*(int*)(D->ptr) << std::endl;
 	
 	return (0);
 }
