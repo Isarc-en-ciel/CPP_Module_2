e9ec3e10fe89aa4f158c6335971e12740d54fa33
iwaslet <iwaslet@student.42.fr>
2026-01-09 16:23:47 +0100

    Cpp

---
+++ Cpp08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iwaslet <iwaslet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/09 16:10:07 by iwaslet           #+#    #+#             */
+/*   Updated: 2026/01/09 16:19:14 by iwaslet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+
+void easyfind(T container, int tofind)
+{
+	
+}
+
+#endif
