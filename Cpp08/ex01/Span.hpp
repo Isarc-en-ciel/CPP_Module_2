bb17430b2f17d7dd750480a7eb4a810cb02eda73
iwaslet <iwaslet@student.42.fr>
2026-01-16 17:10:51 +0100

    Cpp

--- Cpp08/ex01/Span.hpp
+++ Cpp08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: iwaslet <iwaslet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 16:12:48 by iwaslet           #+#    #+#             */
-/*   Updated: 2026/01/09 16:14:32 by iwaslet          ###   ########.fr       */
+/*   Updated: 2026/01/16 16:59:04 by iwaslet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,89 @@
 #define SPAN_HPP
 
 #include <iostream>
+#include <iterator>
+#include <vector>
+
+class Span
+{
+	private:
+	unsigned int _size;
+	std::vector<int> vect;
+	public:
+	Span()
+	{
+		this->_size = 0;
+		std::vector<int> vect;
+		return;
+	};
+	Span(unsigned int N)
+	{
+		this->_size = N;
+		std::vector<int> vect;
+		return;
+	};
+	~Span()
+	{};
+	class NoSpan : public std::exception
+	{
+		virtual const char *what() const throw();
+	};
+	class LimitReached : public std::exception
+	{
+		virtual const char *what() const throw();
+	};
+	void addNumber(int toAdd)
+	{
+		if (this->_size == 0)
+			throw LimitReached();
+		this->vect.push_back(toAdd);
+		this->_size --;
+	};
+	int shortestSpan()
+	{
+		int e = 0;
+		int d = 0;
+		if (vect.size() == 0|| vect.size() == 1)
+			throw NoSpan();
+		for (int j = *vect.end(); vect[j]; j--)
+		{
+			for (int i = 0; vect[i]; i++)
+			{
+				if (i != j)
+					d = std::distance(&vect[i], &vect[j]);
+				if (d < e)
+					e = d;
+			}
+		}
+		return(e);
+	};
+	int longestSpan()
+	{
+		int d = 0;
+		int e = 0;
+		if (vect.size() == 0|| vect.size() == 1)
+			throw NoSpan();
+		for (int j = *vect.end(); vect[j]; j--)
+		{
+			for (int i = 0; vect[i]; i++)
+			{
+				if (i != j)
+					d = std::distance(&vect[i], &vect[j]);
+				if (d > e)
+					e = d;
+			}
+		}
+		return (e);
+	};
+};
+
+const char* Span::NoSpan::what() const throw()
+{
+	return "No span to find";
+}
+const char* Span::LimitReached::what() const throw()
+{
+	return "Maximum elements reached";
+}
 
 #endif
--- Cpp08/ex01/main.cpp
+++ Cpp08/ex01/main.cpp
@@ -6,8 +6,59 @@
 /*   By: iwaslet <iwaslet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 16:10:10 by iwaslet           #+#    #+#             */
-/*   Updated: 2026/01/09 16:12:42 by iwaslet          ###   ########.fr       */
+/*   Updated: 2026/01/16 16:50:47 by iwaslet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+
+int main()
+{
+	try
+	{
+		Span mySpan(5);
+		mySpan.addNumber(15);
+		mySpan.addNumber(47);
+		mySpan.addNumber(25);
+		mySpan.addNumber(39);
+		mySpan.addNumber(82);
+		mySpan.addNumber(73);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Span mySpan(5);
+		mySpan.addNumber(15);
+		mySpan.addNumber(47);
+		mySpan.addNumber(25);
+		mySpan.addNumber(39);
+		mySpan.addNumber(82);
+		std::cout << mySpan.shortestSpan() << std::endl;
+		std::cout << mySpan.longestSpan() << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}try
+	{
+		Span mySpan(1);
+		mySpan.addNumber(15);
+		mySpan.shortestSpan();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	try
+	{
+		Span mySpan(5);
+		mySpan.shortestSpan();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
